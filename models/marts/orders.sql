505b19764f17893894471cf94afca2d4404a7be9
deepa-balwally <218080436+dee0407@users.noreply.github.com>
2025-09-08 21:46:24 +0530

    Reverting changes made

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -50,13 +50,7 @@ compute_booleans as (
         order_items_summary.count_drink_items,
         order_items_summary.count_order_items,
         order_items_summary.count_food_items > 0 as is_food_order,
-        order_items_summary.count_drink_items > 0 as is_drink_order,
-
-        -- New column to flag holiday orders
-        case
-            when date_trunc('month', ordered_at) = '2023-12-01' then true
-            else false
-        end as is_holiday_order
+        order_items_summary.count_drink_items > 0 as is_drink_order
 
     from orders
 
